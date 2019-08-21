9f93fa7dcb910afee5e6dfdb8e68afdc31bfef4d
rickynyairo <rickynyairo@gmail.com>
2019-08-21 22:08:53 +0300

    chore(CI/CD): set up circle-ci to run containers

--- src/services/database.ts
+++ src/services/database.ts
@@ -1,8 +1,11 @@
 import { connect, connection } from "mongoose";
 import config from "../config";
 
-export const connectToDatabase = () => {
+export const connectToDatabase = async () => {
   try {
+    // const AURL = "mongodb://sms-management-api_mongo_1:27017/sms-api";
+    // const trial = await connect(`${config.MONGO_DB_URL}`, { useNewUrlParser: true });
+    // console.log("trial>>>>", trial.connections[0]);
     return connect(`${config.MONGO_DB_URL}`, { useNewUrlParser: true });
   } catch (error) {
     console.log("\nerror>>>\n", error);
